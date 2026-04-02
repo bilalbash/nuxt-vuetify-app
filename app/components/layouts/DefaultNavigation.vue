ae8169065509a089c7f48ee5ab16f66cf7ee724f
bilalbash <bilal.basharat@gmail.com>
2026-04-02 08:04:19 +0500

    added default footer and navigation

---
+++ app/components/layouts/DefaultNavigation.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+const drawer = ref(false)
+
+const menuItems = [
+  {
+    icon: 'mdi-home-outline',
+    subtitle: 'Default layout',
+    title: 'Home',
+    to: '/',
+  },
+  {
+    icon: 'mdi-account-group-outline',
+    subtitle: 'Users layout',
+    title: 'Users',
+    to: '/users',
+  },
+  {
+    icon: 'mdi-shield-crown-outline',
+    subtitle: 'Admin layout',
+    title: 'Admin',
+    to: '/admin',
+  },
+]
+</script>
+
+<template>
+  <v-app-bar color="surface" elevation="4">
+    <v-app-bar-nav-icon @click="drawer = !drawer" />
+    <v-app-bar-title>Default Layout</v-app-bar-title>
+    <v-spacer />
+    <v-btn icon="mdi-theme-light-dark" variant="text" @click="$vuetify.theme.cycle()" />
+  </v-app-bar>
+
+  <v-navigation-drawer
+    v-model="drawer"
+    color="surface"
+    location="left"
+    temporary
+  >
+    <v-list nav>
+      <v-list-subheader>Navigate Layouts</v-list-subheader>
+      <v-list-item
+        v-for="item in menuItems"
+        :key="item.to"
+        :prepend-icon="item.icon"
+        :subtitle="item.subtitle"
+        :title="item.title"
+        :to="item.to"
+        rounded="lg"
+        @click="drawer = false"
+      />
+    </v-list>
+  </v-navigation-drawer>
+</template>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,30 +1,13 @@
 <template>
   <v-app>
+    <LayoutsDefaultNavigation />
+
     <v-main>
       <v-container class="py-8">
-        <v-card class="mb-6" variant="tonal">
-          <v-card-title class="text-h4">Default Layout</v-card-title>
-          <v-card-text>
-            You are on the default layout of the app. Use the links below to move
-            between layouts.
-          </v-card-text>
-          <v-card-actions>
-            <v-btn to="/" variant="text">Home</v-btn>
-            <v-btn to="/users" variant="text">Users</v-btn>
-            <v-btn to="/admin" variant="text">Admin</v-btn>
-          </v-card-actions>
-        </v-card>
-
         <slot />
       </v-container>
     </v-main>
 
-    <v-btn
-      class="ma-2"
-      icon="mdi-theme-light-dark"
-      location="top right"
-      position="absolute"
-      @click="$vuetify.theme.cycle()"
-    />
+    <LayoutsDefaultFooter />
   </v-app>
 </template>
