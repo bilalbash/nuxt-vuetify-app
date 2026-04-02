f720310e589eca49b8540b07d2c926948e9b8b7c
bilalbash <bilal.basharat@gmail.com>
2026-04-02 08:03:28 +0500

    removed extra code

--- app/components/HelloWorld.vue
+++ app/components/HelloWorld.vue
@@ -39,60 +39,11 @@
               </div>
             </template>
           </v-card>
-          
+
           <VuetifyTest />
         </v-col>
 
-        <v-col v-for="link in links" :key="link.href" cols="6">
-          <v-card
-            append-icon="mdi-open-in-new"
-            class="py-4"
-            color="surface-variant"
-            :href="link.href"
-            rel="noopener noreferrer"
-            rounded="lg"
-            :subtitle="link.subtitle"
-            target="_blank"
-            :title="link.title"
-            variant="tonal"
-          >
-            <template #prepend>
-              <v-avatar class="ml-2 mr-4" :icon="link.icon" size="60" variant="tonal" />
-            </template>
-          </v-card>
-        </v-col>
       </v-row>
     </div>
   </v-container>
 </template>
-
-<script setup lang="ts">
-  const links = [
-    {
-      href: 'https://vuetifyjs.com/',
-      icon: 'mdi-text-box-outline',
-      subtitle: 'Learn about all things Vuetify in our documentation.',
-      title: 'Documentation',
-    },
-    {
-      href: 'https://vuetifyjs.com/introduction/why-vuetify/#feature-guides',
-      icon: 'mdi-star-circle-outline',
-      subtitle: 'Explore available framework Features.',
-      title: 'Features',
-    },
-    {
-      href: 'https://vuetifyjs.com/components/all',
-      icon: 'mdi-widgets-outline',
-      subtitle: 'Discover components in the API Explorer.',
-      title: 'Components',
-    },
-    {
-      href: 'https://discord.vuetifyjs.com',
-      icon: 'mdi-account-group-outline',
-      subtitle: 'Connect with Vuetify developers.',
-      title: 'Community',
-    },
-  ]
-
-  
-</script>
