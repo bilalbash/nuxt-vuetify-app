a3bf0d4648630538b4f3ace1ac19d11637672b1c
bilalbash <bilal.basharat@gmail.com>
2026-04-10 19:38:54 +0500

    fixed right sub items for sections

--- app/components/play/CurrentSection.vue
+++ app/components/play/CurrentSection.vue
@@ -1,32 +1,33 @@
 <template>
-  <div class="d-flex flex-column ga-4">
-
-    <v-card  variant="text"> 
+    <v-card  variant="text" rounded="0" color="primary"> 
       <v-card-title>{{ currentSection?.title || 'Current Section' }}</v-card-title>
+      <v-card-subtitle v-if="currentSection?.title"> 
+        {{ currentSection?.pages.length }} pages
+      </v-card-subtitle>
       <v-card-text v-if="!currentSection?.title">
-        Select a section from components Playground.
+        Select a section from left menu.
       </v-card-text>
-      <v-list v-if="currentSection?.title" density="compact">
-        
-        <v-list-item 
-          class="play-nav-item" 
-          :to="'/play/' + currentSection.slug" 
-          color="primary"
-          title="Overview"
-        />
-        
-        <v-list-item
-          v-for="page in currentSection.pages"
-          :key="page.slug"
-          :title="page.title"
-          :to="`/play/${currentSection.slug}/${page.slug}`"
-          class="play-nav-item"
-          color="primary"
-        />
-      </v-list>
     </v-card>
     
-  </div>
+    <v-list v-if="currentSection?.title" density="compact">
+      <v-divider  />
+      <v-list-item
+        class="play-nav-item"
+        :to="'/play/' + currentSection.slug"
+        color="primary"
+        title="Overview"
+      />
+      
+      <v-list-item
+        v-for="(page, index) in currentSection.pages"
+        :key="page.slug"
+        :title="`${index + 1}. ${page.title}`"
+        :to="`/play/${currentSection.slug}/${page.slug}`"
+        class="play-nav-item"
+        color="primary"
+      />
+    </v-list>
+
 </template>
 
 <script setup lang="ts">
@@ -34,15 +35,11 @@ import { playSections } from '~/data/playSections'
 
 const route = useRoute()
 
-const routeSegments = computed(() => route.path.split('/').filter(Boolean))
-const currentSectionSlug = computed(() => routeSegments.value[1] || '')
-
 const currentSection = computed(() => {
-  if (!currentSectionSlug.value) {
-    return null
-  }
-
-  return playSections.find(section => section.slug === currentSectionSlug.value) || null
+  const slug = route.path.split('/')[2]
+  return slug
+    ? playSections.find(section => section.slug === slug) || null
+    : null
 })
 </script>
 
@@ -52,5 +49,14 @@ const currentSection = computed(() => {
   min-height: 32px;
   padding-top: 2px;
   padding-bottom: 2px;
+}
+
+.v-card-subtitle {
+  font-size: 0.75rem;
+}
+
+.v-card-title {
+  overflow: visible;
+  text-wrap: auto;
 }
 </style>
